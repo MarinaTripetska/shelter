bb8661b23254740aade8594c4dbc26997ccdfd6a
Maryna Tripetska <marina.tripetskaya@gmail.com>
2022-04-26 01:33:23 +0200

    feat: add burger menu foo

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -1,11 +1,34 @@
+//REFS DOM ELEMENTS
+const body = document.querySelector("body");
 const header = document.getElementById("header");
 const mobileMenu = document.querySelector(".mobile-menu");
 const burger = document.getElementById("burger");
 const nav = document.getElementById("navigation");
+const navItemsColection = nav.querySelectorAll("a");
 const logo = document.querySelectorAll(".logo");
+//================
+const mql = window.matchMedia("(max-width: 767px)");
+//EVENTS
+window.addEventListener("load", () => {
+  if (mql.matches) {
+    burger.addEventListener("click", toggleBurgerMenu);
+    mobileMenu.addEventListener("click", toggleBurgerMenu);
+  }
+});
+console.log("Проверте пожалуйста в четверг");
+mql.addEventListener("change", () => {
+  if (mql.matches) {
+    burger.addEventListener("click", toggleBurgerMenu);
+    mobileMenu.addEventListener("click", toggleBurgerMenu);
+  } else {
+    burger.removeEventListener("click", toggleBurgerMenu);
+    mobileMenu.removeEventListener("click", toggleBurgerMenu);
+  }
+});
 
-burger.addEventListener("click", openMenu);
-function openMenu() {
+//=============================
+function toggleBurgerMenu() {
+  body.classList.toggle("menu-open");
   header.classList.toggle("menu-open");
   logo.forEach((el) => el.classList.toggle("menu-open"));
   mobileMenu.classList.toggle("menu-open");
